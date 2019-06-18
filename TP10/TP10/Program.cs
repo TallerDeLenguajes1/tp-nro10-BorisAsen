9fa69269e30ba45d620362d20a3e371612c3220d
BorisAsen <borisyasen@gmail.com>
2019-06-18 12:30:37 -0300

    TP10 terminado

---
+++ TP10/TP10/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP10
+{
+    public enum TipoDeOperacion { Venta, Alquiler };
+    public enum TipoDePropiedad { Departamento, Casa, Duplex, Penthouse, Terreno };
+    public enum Calles { SanJuan, Santiago, Mendoza, AvRoca, AvIndependencia, Suipacha };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear lista de inmuebles
+            List<Property> Properties = new List<Property>();
+
+            //Cargar los datos de los inmuebles en la lista
+            Console.WriteLine(" Propiedades Cargadas desde csv: ");
+            string CsvFile = "propiedades.csv";
+            PropertiesFromCsvToList(CsvFile, Properties);
+
+            //Mostar todos las propiedades por pantalla
+            ShowProperties(Properties);
+
+            //Guardar la nueva lista en un csv
+            string CsvFileToSave = "propiedades guardadas.csv";
+            SaveListInCsv(Properties, CsvFileToSave);
+
+            //Listar los discos y solicitar la unidad en la que se hara el bk
+            Console.WriteLine("\nA continuacion se enlistaran los discos disponobles para el backup:");
+            string bkDest = SelectBackUpPath();
+            BackUp(Properties, bkDest);
+            Console.ReadKey();
+
+        }
+
+        public static void BackUp(List<Property> Properties, string nkDest)
+        {
+            string BackUpPath = nkDest+@"\Backup";
+            string fileNumber = "";
+
+            if (!Directory.Exists(BackUpPath))
+            {
+                Directory.CreateDirectory(BackUpPath);
+            }
+            else
+            {
+                string[] availableFiles = Directory.GetFiles(BackUpPath);
+                fileNumber = Convert.ToString(availableFiles.Length);
+            }
+
+            string BackUpFile = BackUpPath + @"\backup" + fileNumber+".bk";
+
+            FileStream bk = File.Create(BackUpFile);
+            using (StreamWriter bkWriter = new StreamWriter(bk))
+            {
+                string line;
+                foreach (Property prop in Properties)
+                {
+                    line = prop.ConcatenarDatos();
+                    bkWriter.WriteLine(line);
+                }
+                bkWriter.Close();
+            }
+
+            Console.WriteLine("\nArchivo de backup creado: "+BackUpFile);
+        }
+
+
+        public static string SelectBackUpPath()
+        {
+            int i = 1;
+            string[] availableDisks = Environment.GetLogicalDrives();
+            Console.WriteLine();
+            foreach (string drive in availableDisks)
+            {
+                Console.WriteLine("   " + i + ") " + drive);
+                i++;
+            }
+
+            Console.Write("\nIngrese el numero correspondiente al disco elegido: ");
+            int nro = Convert.ToInt16(Console.ReadLine());
+            string selectedDirectory = @availableDisks[nro - 1];
+            Console.WriteLine("\nOpcion seleccionada: " + selectedDirectory);
+
+            return selectedDirectory;
+        }
+
+        public static void PropertiesFromCsvToList(string CsvFile, List<Property> Properties)
+        {
+            string[] content=null;
+            try
+            {
+                content = File.ReadAllLines(CsvFile);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("\n¡Hubo un error!\n");
+                Console.WriteLine(error.Message);   
+            }
+            
+            Property new_p;
+
+            foreach (string line in content)
+            {
+                new_p = NewProperty(line);
+                Properties.Add(new_p);
+            }
+        }
+
+        public static Random rand = new Random();
+
+        public static Property NewProperty(string line)
+        {
+            string[] lineContent = line.Split(';');
+
+
+            Property new_p = new Property();
+
+            int _Id = rand.Next(100);
+            string _TipoDePropiedad  = lineContent[1];
+            //string _TipoDePropiedad = Enum.GetName(typeof(TipoDePropiedad), rand.Next(5)); //Del 0 al 4
+            string _TipoDeOperacion = Enum.GetName(typeof(TipoDeOperacion), rand.Next(2)); //Del 0 al 4;
+            float _Tamanio = rand.Next(1001)+1;
+            int _CantidadDeBanios = rand.Next(5)+1;//1 al 5
+            int _CantidadDeHabitaciones = rand.Next(10) + 1;//1 al 10;
+            string _Domicilio = lineContent[0];
+            //string _Domicilio = Enum.GetName(typeof(Calles), rand.Next(5)) +" "+ Convert.ToString(rand.Next(1001));
+            int _Precio = rand.Next(2000000);
+            bool BoolAux;
+            if (rand.Next(2)==1)
+            {
+                BoolAux = true;
+            }
+            else
+            {
+                BoolAux = false;
+            }
+            bool _Estado = BoolAux;
+
+            new_p.Id = _Id;
+            new_p.TipoDePropiedad = _TipoDePropiedad;
+            new_p.TipoDeOperacion = _TipoDeOperacion;
+            new_p.Tamanio = _Tamanio;
+            new_p.CantidadDeBanios = _CantidadDeBanios;
+            new_p.CantidadDeHabitaciones = _CantidadDeHabitaciones;
+            new_p.Domicilio = _Domicilio;
+            new_p.Precio = _Precio;
+            new_p.Estado = _Estado;
+
+            return new_p;
+
+        }
+
+        public static void ShowProperties(List<Property> Properties)
+        {
+            foreach (Property pro in Properties)
+            {
+                pro.ShowProperty();
+            }
+        }
+
+        public static void SaveListInCsv(List<Property> Properties, string CsvFileToSave)
+        {
+            //StreamWriter file = File.CreateText(CsvFileToSave);
+            string line;
+
+            using (StreamWriter file = File.CreateText(CsvFileToSave))
+            {
+                foreach (Property prop in Properties)
+                {
+                    line = prop.ConcatenarDatos();
+                    file.WriteLine(line);
+                }
+                file.Close();
+            }
+
+            
+
+            
+        }
+    }
+}
---
+++ TP10/TP10/Propiedad.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP10
+{
+    class Property
+    {
+        int _Id;
+        string _TipoDePropiedad;
+        string _TipoDeOperacion;
+        float _Tamanio;
+        int _CantidadDeBanios;
+        int _CantidadDeHabitaciones;
+        string _Domicilio;
+        float _Precio;
+        bool _Estado;//true=activo false=inactivo
+        //float _ValorDelInmueble;
+
+        public int Id
+        {
+            get
+            {
+                return _Id;
+            }
+
+            set
+            {
+                _Id = value;
+            }
+        }
+
+        public string TipoDePropiedad
+        {
+            get
+            {
+                return _TipoDePropiedad;
+            }
+
+            set
+            {
+                _TipoDePropiedad = value;
+            }
+        }
+
+        public string TipoDeOperacion
+        {
+            get
+            {
+                return _TipoDeOperacion;
+            }
+
+            set
+            {
+                _TipoDeOperacion = value;
+            }
+        }
+
+        public float Tamanio
+        {
+            get
+            {
+                return _Tamanio;
+            }
+
+            set
+            {
+                _Tamanio = value;
+            }
+        }
+
+        public int CantidadDeBanios
+        {
+            get
+            {
+                return _CantidadDeBanios;
+            }
+
+            set
+            {
+                _CantidadDeBanios = value;
+            }
+        }
+
+        public int CantidadDeHabitaciones
+        {
+            get
+            {
+                return _CantidadDeHabitaciones;
+            }
+
+            set
+            {
+                _CantidadDeHabitaciones = value;
+            }
+        }
+
+        public string Domicilio
+        {
+            get
+            {
+                return _Domicilio;
+            }
+
+            set
+            {
+                _Domicilio = value;
+            }
+        }
+
+        public float Precio
+        {
+            get
+            {
+                return _Precio;
+            }
+
+            set
+            {
+                _Precio = value;
+            }
+        }
+
+        public bool Estado
+        {
+            get
+            {
+                return _Estado;
+            }
+
+            set
+            {
+                _Estado = value;
+            }
+        }
+
+        public void ShowProperty()
+        {
+            Console.WriteLine("\n\n* Inmueble (Id "+_Id+") :");
+            Console.WriteLine("    Tipo de propiedad: " + _TipoDePropiedad);
+            Console.WriteLine("    Tipo de operacion: " + _TipoDeOperacion);
+            Console.WriteLine("    Tamaño: " + _Tamanio + "m2");
+            Console.WriteLine("    Cantidad de baños: " + _CantidadDeBanios);
+            Console.WriteLine("    Cantidad de habitaciones: " + _CantidadDeHabitaciones);
+            Console.WriteLine("    Domicilio: " + _Domicilio);
+            Console.WriteLine("    Precio: $" + _Precio);
+            if (_Estado)
+            {
+                Console.WriteLine("    Estado: Activo ");
+            }
+            else
+            {
+                Console.WriteLine("    Estado: Inactivo ");
+            }
+            
+            Console.WriteLine("    Valor del Inmueble: $" + ValorDelInmueble());
+
+        }
+
+        double ValorDelInmueble()
+        {
+            double PrecioFinal = 0;
+            if (_TipoDeOperacion == "Venta")
+            {
+                PrecioFinal = _Precio + (_Precio * 0.21) + (_Precio * 0.10) + 10000;
+            }
+
+            if (_TipoDeOperacion == "Alquiler")
+            {
+                PrecioFinal = (_Precio * 0.02) + (_Precio * 0.5);
+            }
+
+            return PrecioFinal;
+        }
+
+        public string ConcatenarDatos()
+        {
+            string aux;
+            if (_Estado)
+            {
+                aux = "Activo";
+            }
+            else
+            {
+                aux = "Inactivo";
+            }
+            return _Id + ";" + _TipoDePropiedad + ";" + _TipoDeOperacion + ";" + _Tamanio + ";" + _CantidadDeBanios + ";" + _CantidadDeHabitaciones + ";" + _Domicilio + ";" + _Precio + ";" + aux+";"+ ValorDelInmueble();
+        }
+    }
+}
